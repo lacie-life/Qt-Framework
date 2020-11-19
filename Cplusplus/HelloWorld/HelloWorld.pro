aa1f4ddbf7e2e5b946e271b15b53ce09d786ea61
lacie-life <00sao00ios00@gmail.com>
2020-11-19 22:38:44 +0700

    update code

--- Cplusplus/HelloWorld/HelloWorld.pro
+++ Cplusplus/HelloWorld/HelloWorld.pro
@@ -1,4 +1,5 @@
 QT -= gui
+QT += core
 
 CONFIG += c++11 console
 CONFIG -= app_bundle
--- Cplusplus/HelloWorld/main.cpp
+++ Cplusplus/HelloWorld/main.cpp
@@ -1,8 +1,9 @@
-#include <QCoreApplication>
+#include <QtCore>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    std::cout << "Qt verrsion: " << qVersion() << std::endl;
 
-    return a.exec();
+    return 0;
 }
