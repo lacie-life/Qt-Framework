6415cd69baa0a153c7c79ab635e810be7a165772
lacie-life <00sao00ios00@gmail.com>
2021-02-17 19:18:30 +0700

    HAPPY NEW YEAR

---
+++ happy_new_year/happy_new_year.cpp
@@ -0,0 +1,12 @@
+﻿// happy_new_year.cpp : Defines the entry point for the application.
+//
+
+#include "happy_new_year.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "HAPPY NEW YEAR" << endl;
+	return 0;
+}
