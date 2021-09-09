5ee036a08bdabc64b7b96d9c906dc0344c959407
lacie-life <00sao00ios00@gmail.com>
2021-09-10 00:26:55 +0700

    |Update| Qt3DPointCloud

--- MiniProject/build-QuickVtkExample-Desktop_Qt_5_15_2_GCC_64bit-Debug/VtkFboInQtQuickConfig.h
+++
@@ -1,7 +0,0 @@
-#ifndef VTKFBOINQTQUICKCONFIG_H_IN
-#define VTKFBOINQTQUICKCONFIG_H_IN
-
-#define PROJECT_SOURCE_DIR "/home/lacie-life/Github/QtQuick-QML/MiniProject/QuickVtkExample"
-#define PROJECT_BINARY_DIR "/home/lacie-life/Github/QtQuick-QML/MiniProject/build-QuickVtkExample-Desktop_Qt_5_15_2_GCC_64bit-Debug"
-
-#endif // VTKFBOINQTQUICKCONFIG_H_IN
