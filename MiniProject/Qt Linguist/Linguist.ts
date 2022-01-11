d61ec82e90c911267f929430ffbb8d6c9588d07c
lacie-life <00sao00ios00@gmail.com>
2022-01-11 16:32:43 +0700

    [Update] update qt linguist example

---
+++ MiniProject/Qt Linguist/Linguist.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="45"/>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="64"/>
+        <source>New</source>
+        <translation>新文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="69"/>
+        <source>Save</source>
+        <translation>保存文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="74"/>
+        <source>Open</source>
+        <translation>打开文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="79"/>
+        <source>Quit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="84"/>
+        <source>Copy</source>
+        <translation>复制</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <source>Paste</source>
+        <translation>粘贴</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="94"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="99"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="18"/>
+        <source>What is your name?</source>
+        <translation>你叫什么名字？</translation>
+    </message>
+</context>
+</TS>
